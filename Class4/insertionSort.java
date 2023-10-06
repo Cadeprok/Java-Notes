a094e7d8cf7ad3cfa4a3ed30ea5e8b10975064bc
Cadeprok <prokopc1@montclair.edu>
2023-10-05 21:29:59 -0400

    more insertion sort stuff

--- Class4/insertionSort.java
+++ Class4/insertionSort.java
@@ -15,6 +15,20 @@ public class insertionSort {
         }
         // return array;
     }
+
+    public int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > temp) { // Compares item to the left -- if item on left is greater than item on
+                                                // right, then swap them
+                array[j + 1] = array[j]; // swapping them
+                j--; // subtracting to not repeat swapping/ runtime
+            }
+            array[j + 1] = temp; // original value that was being moved
+        }
+        return array;
+    }
 }
 
 // For explaining compareTo()
